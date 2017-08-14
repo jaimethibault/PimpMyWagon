451697c7e8e36820ef6410adeb458dee6c03f224
Thibault Jaime <jaime.thibault@gmail.com>
2017-08-14 14:31:38 +0200

    installed devise gem for signin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer configuration
+  config.action_mailer.default_url_options = { host: 'https://airbnb-jaimethibault.herokuapp.com/' }
 end
