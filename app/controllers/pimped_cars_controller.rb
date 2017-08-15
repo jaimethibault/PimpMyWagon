aa6739a8edfe4e4afcfce10ad4303297505a221c
Clemence Fernet <clemence.fernet@hotmail.fr>
2017-08-15 11:47:14 +0200

    added new & create pimped car functionality

--- app/controllers/pimped_cars_controller.rb
+++ app/controllers/pimped_cars_controller.rb
@@ -1,5 +1,5 @@
 class PimpedCarsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new]
   before_action :set_pimped_cars, only: [:show]
 
   def index
@@ -9,9 +9,24 @@ class PimpedCarsController < ApplicationController
   def show
   end
 
+  def new
+    @pimped_car = PimpedCar.new
+  end
+
+  def create
+   @pimped_car = PimpedCar.new(set_params)
+   @pimped_car.user_id = current_user.id
+   @pimped_car.save
+   redirect_to pimped_cars_path(@pimped_car)
+  end
+
   private
 
+  def set_params
+    params.require(:pimped_car).permit(:name, :price_per_day, :description)
+  end
+
   def set_pimped_cars
-    @pimpedcar = PimpedCar.find(params[:id])
+    @pimped_car = PimpedCar.find(params[:id])
   end
 end
