77574f2c00d20e5100ce2bad0dfea44134b17e18
Clemence Fernet <clemence.fernet@hotmail.fr>
2017-08-16 15:05:19 +0200

    updated root with index for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
+  root to: 'pimped_cars#index'
   resources :pimped_cars, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:create]
   end
