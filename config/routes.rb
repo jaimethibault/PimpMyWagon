332204718e4edd277b421173ae1f5c0904ab24e1
Dima Muasses <dima.muasses7@gmail.com>
2017-08-17 07:36:51 -0400

    home video / suppression navbar et footer / scss + js home video

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pimped_cars#index'
+  root to: 'pages#home'
   resources :pimped_cars, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:create]
   end
