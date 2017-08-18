9a402a571426ae8a72f15f1f0a598731a46094dc
Thibault Jaime <jaime.thibault@gmail.com>
2017-08-18 15:57:27 +0200

    Revert "Redesign show"

--- Gemfile
+++ Gemfile
@@ -37,5 +37,3 @@ group :development, :test do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
-
-gem 'bootstrap-datepicker-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require bootstrap-datepicker
 //= require underscore
 //= require gmaps/google
+//= require_tree .
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
-//= require_tree .
